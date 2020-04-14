512808bd96a21ff3cd87b4dfcf4770abe6a047da
Varsine <varsulmozg@gmail.com>
2020-04-14 18:00:23 +0400

    Updates

---
+++ precache-manifest.673f0a7973d9e08343b3ec9062e36f2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "373fb9e8eb8df367ad5bd33cd4c5670f",
+    "url": "/firebase-react/index.html"
+  },
+  {
+    "revision": "6ca1e7506c9a130b3313",
+    "url": "/firebase-react/static/css/main.4c1d74e2.chunk.css"
+  },
+  {
+    "revision": "e0279ab83e3e52a32783",
+    "url": "/firebase-react/static/js/2.2c520e3f.chunk.js"
+  },
+  {
+    "revision": "f2711bdfd365598ccdad5ea4cf24e3ae",
+    "url": "/firebase-react/static/js/2.2c520e3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ca1e7506c9a130b3313",
+    "url": "/firebase-react/static/js/main.f3d11559.chunk.js"
+  },
+  {
+    "revision": "5cf8f8e4b5c12b0ce1be",
+    "url": "/firebase-react/static/js/runtime-main.583fda1e.js"
+  }
+]);
