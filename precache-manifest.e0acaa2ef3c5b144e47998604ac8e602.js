007fa757ad256163200164c85bd9129fcd4119a1
Varsine <varsulmozg@gmail.com>
2020-04-15 14:50:15 +0400

    Updates

---
+++ precache-manifest.e0acaa2ef3c5b144e47998604ac8e602.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44cb67333613ccc2ab5ed1af066c4030",
+    "url": "/firebase-react/index.html"
+  },
+  {
+    "revision": "d0a243d58349a9fb85d1",
+    "url": "/firebase-react/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "8be61d601ec1fc599276",
+    "url": "/firebase-react/static/css/main.a0fed7ee.chunk.css"
+  },
+  {
+    "revision": "d0a243d58349a9fb85d1",
+    "url": "/firebase-react/static/js/2.9c6c0ab2.chunk.js"
+  },
+  {
+    "revision": "fe412843ac0312c2418e191fccf2a6de",
+    "url": "/firebase-react/static/js/2.9c6c0ab2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8be61d601ec1fc599276",
+    "url": "/firebase-react/static/js/main.58cd4bb5.chunk.js"
+  },
+  {
+    "revision": "5cf8f8e4b5c12b0ce1be",
+    "url": "/firebase-react/static/js/runtime-main.583fda1e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/firebase-react/precache-manifest.3fad0fc891750130cdf500b0f51459e8.js"
+  "/firebase-react/precache-manifest.e0acaa2ef3c5b144e47998604ac8e602.js"
 );
 
 self.addEventListener('message', (event) => {
